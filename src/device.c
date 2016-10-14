a4bc2c596e8bc599253ee28e7dce47b56c48768c
Jason A. Donenfeld <Jason@zx2c4.com>
2016-10-14 17:38:16 +0200

    device: show debug message when no peer has allowed-ips for packet

--- src/device.c
+++ src/device.c
@@ -112,6 +112,7 @@ static netdev_tx_t xmit(struct sk_buff *skb, struct net_device *dev)
 
 	peer = routing_table_lookup_dst(&wg->peer_routing_table, skb);
 	if (unlikely(!peer)) {
+		net_dbg_ratelimited("No peer is configured for outgoing packet address\n");
 		skb_unsendable(skb, dev);
 		return -ENOKEY;
 	}
