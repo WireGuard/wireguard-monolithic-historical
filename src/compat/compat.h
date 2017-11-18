e4b387551cc3b7d47e3d000b028aa0732f819ada
Jason A. Donenfeld <Jason@zx2c4.com>
2017-11-18 23:13:11 +0100

    compat: stable finally backported fix

--- src/compat/compat.h
+++ src/compat/compat.h
@@ -451,7 +451,7 @@ static inline struct nlattr **genl_family_attrbuf(const struct genl_family *fami
 #define COMPAT_CANNOT_USE_GENL_NOPS
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0)
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 13, 14) && LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)) || LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 63)
 #define get_device_dump(a, b) get_device_dump_real(a, b); \
 static int get_device_dump(a, b) { \
 	int ret; \
