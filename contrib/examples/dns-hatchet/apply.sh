58cf40ccf4319d3b6285f3b537c25605f7016803
Jason A. Donenfeld <Jason@zx2c4.com>
2018-05-15 21:48:41 +0200

    dns-hatchet: update paths
    Suggested-by: Martin Hauke <mardnh@gmx.de>

--- contrib/examples/dns-hatchet/apply.sh
+++ contrib/examples/dns-hatchet/apply.sh
@@ -6,4 +6,4 @@
 ME="$(readlink -f "$(dirname "$(readlink -f "$0")")")"
 TOOLS="$ME/../../../src/tools"
 
-sed -i "/~~ function override insertion point ~~/r $ME/hatchet.bash" "$TOOLS/wg-quick.bash"
+sed -i "/~~ function override insertion point ~~/r $ME/hatchet.bash" "$TOOLS/wg-quick/linux.bash"
