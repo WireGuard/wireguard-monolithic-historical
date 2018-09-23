bcec13ea5a01f88e4765ef22636b356df0938fa3
Jason A. Donenfeld <Jason@zx2c4.com>
2018-09-23 17:34:34 +0200

    chacha20-arm: switch to entirely Andy Polyakov's implementation

--- src/crypto/zinc/chacha20/chacha20-arm-glue.h
+++ src/crypto/zinc/chacha20/chacha20-arm-glue.h
@@ -12,9 +12,6 @@
 
 asmlinkage void chacha20_arm(u8 *out, const u8 *in, const size_t len,
 			     const u32 key[8], const u32 counter[4]);
-#if defined(CONFIG_ARM)
-asmlinkage void hchacha20_arm(const u32 state[16], u32 out[8]);
-#endif
 #if defined(CONFIG_KERNEL_MODE_NEON)
 asmlinkage void chacha20_neon(u8 *out, const u8 *in, const size_t len,
 			      const u32 key[8], const u32 counter[4]);
@@ -60,27 +57,5 @@ static inline bool chacha20_arch(struct chacha20_ctx *state, u8 *dst,
 static inline bool hchacha20_arch(u8 *derived_key, const u8 *nonce,
 				  const u8 *key, simd_context_t *simd_context)
 {
-#if defined(CONFIG_ARM)
-	u32 x[] = { CHACHA20_CONSTANT_EXPA,
-		    CHACHA20_CONSTANT_ND_3,
-		    CHACHA20_CONSTANT_2_BY,
-		    CHACHA20_CONSTANT_TE_K,
-		    get_unaligned_le32(key + 0),
-		    get_unaligned_le32(key + 4),
-		    get_unaligned_le32(key + 8),
-		    get_unaligned_le32(key + 12),
-		    get_unaligned_le32(key + 16),
-		    get_unaligned_le32(key + 20),
-		    get_unaligned_le32(key + 24),
-		    get_unaligned_le32(key + 28),
-		    get_unaligned_le32(nonce + 0),
-		    get_unaligned_le32(nonce + 4),
-		    get_unaligned_le32(nonce + 8),
-		    get_unaligned_le32(nonce + 12)
-	};
-	hchacha20_arm(x, (u32 *)derived_key);
-	return true;
-#else
 	return false;
-#endif
 }
