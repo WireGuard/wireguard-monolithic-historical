b4ba33e33b6d64913c08ed1809e711ce98422454
Jason A. Donenfeld <Jason@zx2c4.com>
2018-10-06 06:01:11 +0200

    compat: account for ancient ARM assembler

--- src/compat/compat-asm.h
+++ src/compat/compat-asm.h
@@ -8,11 +8,36 @@
 
 #include <linux/linkage.h>
 #include <linux/kconfig.h>
+#include <linux/version.h>
 
 /* PaX compatibility */
 #if defined(RAP_PLUGIN)
 #undef ENTRY
 #define ENTRY RAP_ENTRY
+#endif
+
+#if defined(__LINUX_ARM_ARCH__) && LINUX_VERSION_CODE < KERNEL_VERSION(3, 17, 0)
+	.irp	c,,eq,ne,cs,cc,mi,pl,vs,vc,hi,ls,ge,lt,gt,le,hs,lo
+	.macro	ret\c, reg
+#if __LINUX_ARM_ARCH__ < 6
+	mov\c	pc, \reg
+#else
+	.ifeqs	"\reg", "lr"
+	bx\c	\reg
+	.else
+	mov\c	pc, \reg
+	.endif
+#endif
+	.endm
+	.endr
+#endif
+
+#if defined(__LINUX_ARM_ARCH__) && LINUX_VERSION_CODE < KERNEL_VERSION(3, 15, 0)
+#include <asm/assembler.h>
+#define lspush push
+#define lspull pull
+#undef push
+#undef pull
 #endif
 
 #endif /* _WG_COMPATASM_H */
