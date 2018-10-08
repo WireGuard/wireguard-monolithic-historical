b3a5d8a84df44905cdc476ff650c4380f41b4bc4
Jason A. Donenfeld <Jason@zx2c4.com>
2018-10-08 22:23:58 +0200

    main: change module description
    Suggested-by: Jiri Pirko <jiri@resnulli.us>

--- src/main.c
+++ src/main.c
@@ -63,7 +63,7 @@ static void __exit mod_exit(void)
 module_init(mod_init);
 module_exit(mod_exit);
 MODULE_LICENSE("GPL v2");
-MODULE_DESCRIPTION("Fast, modern, and secure VPN tunnel");
+MODULE_DESCRIPTION("WireGuard secure network tunnel");
 MODULE_AUTHOR("Jason A. Donenfeld <Jason@zx2c4.com>");
 MODULE_VERSION(WIREGUARD_VERSION);
 MODULE_ALIAS_RTNL_LINK(KBUILD_MODNAME);
