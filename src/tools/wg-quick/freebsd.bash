5f59c76d3295efab9640848ff57f57c3e14e713f
Jason A. Donenfeld <Jason@zx2c4.com>
2018-10-12 16:55:35 +0200

    tools: wg-quick: wait for interface to disappear on freebsd

--- src/tools/wg-quick/freebsd.bash
+++ src/tools/wg-quick/freebsd.bash
@@ -137,8 +137,17 @@ del_routes() {
 }
 
 del_if() {
+	local line monitor_pid
 	[[ $HAVE_SET_DNS -eq 0 ]] || unset_dns
+	exec 39< <(exec route -n monitor 2>/dev/null)
+	monitor_pid=$!
 	cmd rm -f "/var/run/wireguard/$INTERFACE.sock"
+	while ifconfig "$INTERFACE" >/dev/null 2>&1; do
+		while read -r line; do
+			[[ $line =~ ^RTM_IFANNOUNCE:.* ]] && break
+		done <&39
+	done
+	kill $monitor_pid
 }
 
 up_if() {
