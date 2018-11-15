0f81a44b37eec6e0b21bb95934d9fb529720591b
Jason A. Donenfeld <Jason@zx2c4.com>
2018-11-15 00:51:41 -0800

    perlasm: avoid rep ret
    The original hardcodes returns as .byte 0xf3,0xc3, aka "rep ret".
    We replace this by "ret". "rep ret" was meant to help with AMD K8
    chips, cf. http://repzret.org/p/repzret. It makes no sense to
    continue to use this kludge for code that won't even run on ancient
    AMD chips.

--- src/crypto/zinc/perlasm/x86_64-xlate.pl
+++ src/crypto/zinc/perlasm/x86_64-xlate.pl
@@ -164,7 +164,7 @@ my %globals;
 		    $epilogue = "movq	8(%rsp),%rdi\n\t" .
 				"movq	16(%rsp),%rsi\n\t";
 		}
-	    	$epilogue . ".byte	0xf3,0xc3";
+		$epilogue . "ret";
 	    } elsif ($self->{op} eq "call" && !$elf && $current_segment eq ".init") {
 		".p2align\t3\n\t.quad";
 	    } else {
