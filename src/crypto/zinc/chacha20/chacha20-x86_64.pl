9d976b1a28006a21b8fe920b9337162a9a8d737b
Jason A. Donenfeld <Jason@zx2c4.com>
2018-12-07 05:46:22 +0100

    chacha20: do not define unused asm function
    This causes RAP to be unhappy, and we're not using it anyway.
    
    Reported-by: Ivan J. <parazyd@dyne.org>

--- src/crypto/zinc/chacha20/chacha20-x86_64.pl
+++ src/crypto/zinc/chacha20/chacha20-x86_64.pl
@@ -280,14 +280,12 @@ my @x=map("\"$_\"",@x);
 
 ########################################################################
 # Generic code path that handles all lengths on pre-SSSE3 processors.
+if(!$kernel) {
 &declare_function("chacha20_ctr32", 64, 5);
 $code.=<<___;
 .cfi_startproc
 	cmp	\$0,$len
 	je	.Lno_data
-___
-if(!$kernel) {
-$code.=<<___;
 	mov	OPENSSL_ia32cap_P+4(%rip),%r9
 ___
 $code.=<<___	if ($avx>2);
@@ -300,7 +298,6 @@ $code.=<<___;
 	test	\$`1<<(41-32)`,%r9d
 	jnz	.Lchacha20_ssse3
 ___
-}
 $code.=<<___;
 	push	%rbx
 .cfi_push	%rbx
@@ -474,6 +471,7 @@ $code.=<<___;
 .cfi_endproc
 ___
 &end_function("chacha20_ctr32");
+}
 
 ########################################################################
 # SSSE3 code path that handles shorter lengths
